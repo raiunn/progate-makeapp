5289404146d5e63aaaec378ef1e4641557e1db6e
梅北文仁 <umekitafumihito@umekitafumihitonomacbook-pro.local>
2018-05-13 17:04:26 +0900

    Initial Commit

---
+++ progateアプリを作ってみよう/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  progateアプリを作ってみよう
+//
+//  Created by 梅北文仁 on 2018/05/13.
+//  Copyright © 2018年 梅北文仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
